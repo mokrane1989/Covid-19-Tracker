e10f79159c8a37cfbf5a65d0b2453b58b08166d8
m.bahmed <m.bahmed@mesrs.dz>
2020-04-18 16:39:11 +0100

    updated server.js configuration

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/covid-statistiques'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/covid-statistiques/index.html'));
-});
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+  });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
